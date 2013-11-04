1a90e4d1caff40e484bdff8327646ac9eea1a9e4
Jesse S <unknown@kiln.example.com>
2013-11-04 10:23:04 +0500

    Initial Commit

---
+++ src/main/java/Jelloeater/StockTicker/App.java
@@ -0,0 +1,13 @@
+package Jelloeater.StockTicker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
