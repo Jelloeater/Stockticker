98f08df4ae9d82701cab823174709609167b9482
Jesse S <unknown@kiln.example.com>
2013-11-04 11:37:39 +0500

    Found some Jsoup info from http://stackoverflow.com/questions/2835505/how-to-scan-a-website-or-page-for-info-and-bring-it-into-my-program/2835555#2835555

--- src/main/java/Jelloeater/StockTicker/App.java
+++ src/main/java/Jelloeater/StockTicker/App.java
@@ -1,13 +1,27 @@
 package Jelloeater.StockTicker;
 
 /**
- * Hello world!
+ * This is a basic stock ticker app. It ticks stocks n stuff
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        String url = "http://stackoverflow.com/questions/2835505";
+        Document document = Jsoup.connect(url).get();
+
+        String question = document.select("#question .post-text").text();
+        System.out.println("Question: " + question);
+
+        Elements answerers = document.select("#answers .user-details a");
+        for (Element answerer : answerers) {
+            System.out.println("Answerer: " + answerer.text());
+        }
     }
+
 }
